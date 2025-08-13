f4cca63265a35d3d0df91c9a774203f86a32ee59
Zheng <zhengg.forever.young@gmail.com>
2025-08-14 06:09:38 +0800

    Update number_bite.py

--- Python/Binary/number_bite.py
+++ Python/Binary/number_bite.py
@@ -1,1 +1,16 @@
+'''
+count the number of bits that are set to 1 in an integer
+'''
 
+def count_bits_1(x):
+    number_bits = 0
+    while x :
+        number_bits += x & 1
+        x >>= 1
+    return number_bits
+
+
+if __name__ == "__main__":
+    x = int(input('Input an integer: '))
+    print(bin(x)[2:])
+    print(count_bits_1(x))
