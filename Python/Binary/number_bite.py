cfe608a432f9d1118a05f0d328606ba9dfedab33
Zheng <zhengg.forever.young@gmail.com>
2025-08-15 06:39:37 +0800

    Update number_bite.py

--- Python/Binary/number_bite.py
+++ Python/Binary/number_bite.py
@@ -1,11 +1,11 @@
 '''
 count the number of bits that are set to 1 in an integer
 '''
-
+# brute-force algorithm
 def count_bits_1(x):
     number_bits = 0
     while x :
-        number_bits += x & 1
+        number_bits += x & 1#
         x >>= 1
     return number_bits
 
