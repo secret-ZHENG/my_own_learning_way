95c5875b1cc6e3881932aa844bd295b77ed9db52
Zheng <zhengg.forever.young@gmail.com>
2025-10-15 07:52:33 +0800

    Update Smallest_even_mutiple.py

--- Python/Leetcode/Smallest_even_mutiple.py
+++ Python/Leetcode/Smallest_even_mutiple.py
@@ -1,15 +1,16 @@
 class Solution:
     def smallestEvenMultiple(self, n: int) -> int:
         return n << (n & 1)
-        # return n if n % 2 ==0 else n*2
-        '''
+        #1. return n if n % 2 ==0 else n*2
+        '''2.
         return (n % 2 + 1) * n
-        ''''''
+        '''
+        '''3.
         if n % 2 == 0 :
             return n
         else :
             return n * 2
         '''
-        '''
+        '''4.
         return (n % 2 + 1) * n
         '''
