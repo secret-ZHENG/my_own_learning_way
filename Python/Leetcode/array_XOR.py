b7f01ebd2c7303fe5355db31fe5f36aa52937e3f
Zheng <zhengg.forever.young@gmail.com>
2025-10-19 17:04:28 +0800

    Update array_XOR.py

--- Python/Leetcode/array_XOR.py
+++ Python/Leetcode/array_XOR.py
@@ -4,7 +4,8 @@
 
 请返回 nums 中所有元素按位异或（XOR）后得到的结果。
 
-1.
+1.Time Flexible O(n)
+Space Flexible O(n)
 class Solution:
     def xorOperation(self, n: int, start: int) -> int:
         end = 0
@@ -13,4 +14,27 @@ class Solution:
         return end
 
 
-2.
+2.Time Flexible O(1)
+Space Flexible O(1)
+C++
+class Solution {
+public:
+    int sumXor(int x){
+        if (x % 4 == 0) {
+            return x;
+        }
+        if (x % 4 == 1) {
+            return 1;
+        }
+        if (x % 4 == 2) {
+            return x+1 ;
+        }
+        return 0 ;
+    }
+    
+    int xorOperation(int n, int start) {
+        int s = start >>1, e = n & start & 1 ;
+        int ret = sumXor(s-1) ^ sumXor(s+n-1);
+        return ret << 1|e;
+    }
+};
